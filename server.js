302cc963aaea0d73c8bbbf0468f3ccc9856d70d8
Taylor Watters <taylorbbigelow@gmail.com>
2023-04-27 19:41:52 -0700

    switches PORT to default to process.env.PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
